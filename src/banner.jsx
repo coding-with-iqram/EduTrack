6a5557d72e0e8d4036dcf128a2a6298c11d3b324
coding-with-iqram <codingwithiqram@gmail.com>
2025-06-21 23:54:40 +0600

    banner code updated

--- src/banner.jsx
+++ src/banner.jsx
@@ -1,7 +1,7 @@
 const braner = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox , Hi</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
